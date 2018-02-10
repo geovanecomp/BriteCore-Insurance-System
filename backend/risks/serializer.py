7390fcbc4e84401e35e765a793d98186d3571806
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 19:25:37 -0200

    Risk type serializer created

---
+++ backend/risks/serializer.py
@@ -0,0 +1,9 @@
+from .models import RiskType
+from rest_framework import serializers
+
+
+# class RiskTypeSerializer(serializers.HyperlinkedModelSerializer):
+class RiskTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RiskType
+        fields =  '__all__'

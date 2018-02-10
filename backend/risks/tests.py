6c177b031a467777a7f155d6adb978530cdca791
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 16:11:06 -0200

    Simple automated test created

--- backend/risks/tests.py
+++ backend/risks/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import RiskType
 
-# Create your tests here.
+class RiskTypeTestCase(TestCase):
+    def setUp(self):
+        RiskType.objects.create(name='test_name')
+
+    def test_risk_types_should_have_name(self):
+        """All risk types must have the name attribute"""
+
+        risk_type = RiskType.objects.get(name='test_name')
+
+        self.assertTrue(risk_type.name == 'test_name')

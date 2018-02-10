f791da6d02a0f43f9b350f0c7f2e353333b79e95
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 11:36:12 -0200

    Database configuration created

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'NAME': 'britecore_insurance_system',
+         'ENGINE': 'django.db.backends.mysql',
+         'USER': 'geovane',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '3306'
+     }
 }
 
 
---
+++ backend/queries/createschema.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA `britecore_insurance_system` DEFAULT CHARACTER SET utf8 ;

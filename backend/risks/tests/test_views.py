9a69d9a03052b567e000ad5dafa15f09802e3325
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 20:58:29 -0200

    Automated tests for get a risk type finisehd

--- backend/risks/tests/test_views.py
+++ backend/risks/tests/test_views.py
@@ -27,3 +27,28 @@ class GetAllRiskTypes(TestCase):
 
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class GetSingleRiskTypeTest(TestCase):
+    """ Test module for GET single risk type API """
+
+    def setUp(self):
+        self.risk_type1 = RiskType.objects.create(name='Test Risk1')
+        self.risk_type2 = RiskType.objects.create(name='Test Risk2')
+
+    def test_get_valid_single_risk_type(self):
+        response = client.get(
+            reverse('get_delete_update_risk_type',  kwargs={'pk': self.risk_type1.pk}))
+
+        risk = RiskType.objects.get(pk=self.risk_type1.pk)
+        serializer = RiskTypeSerializer(risk)
+
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_single_risk_type(self):
+        invalid_id = 999
+        response = client.get(
+            reverse('get_delete_update_risk_type',  kwargs={'pk': invalid_id}))
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

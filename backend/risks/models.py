6c3cbc4291572e1702829cc5da269caf00419a23
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 16:35:52 -0200

    Testing the risk name service

--- backend/risks/models.py
+++ backend/risks/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class RiskType(models.Model):
+    """Defines all risk types"""
     name = models.CharField(max_length=100, null=False)
+
+    def __repr__(self):
+        return self.name + ' risk type is added.'
--- backend/risks/tests.py
+++ backend/risks/tests.py
@@ -2,12 +2,22 @@ from django.test import TestCase
 from .models import RiskType
 
 class RiskTypeTestCase(TestCase):
+    """ Test module for RiskType model """
+
     def setUp(self):
         RiskType.objects.create(name='test_name')
 
-    def test_risk_types_should_have_name(self):
+    def test_risk_types_should_have_attr_name(self):
         """All risk types must have the name attribute"""
 
         risk_type = RiskType.objects.get(name='test_name')
 
+        # TODO: Discover how to check if an attribute exists
         self.assertTrue(risk_type.name == 'test_name')
+
+    def test_risk_type_name(self):
+        """The risk type should has the same inserted name"""
+
+        risk_type = RiskType.objects.get(name='test_name')
+
+        self.assertEqual(risk_type.name, 'test_name')

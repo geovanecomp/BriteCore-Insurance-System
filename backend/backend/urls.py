122653c884556bffd84f964ac40eab7dbc12d6b2
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 19:25:48 -0200

    Risk type routes created

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-# router.register('risk_api', views.GamesViewSet)
+from django.urls import include, path
+from risks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('risk-api', include(router.urls)),
-
+    path('risks/', include('risks.urls'))
 ]
---
+++ backend/risks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+prefix = 'api/v1/risk-types/'
+
+urlpatterns = [
+    # path('', views.index, name='index'),
+    path(prefix+':pk', views.get_delete_update_risk_type, name='get_delete_update_risk_type'),
+    path(prefix, views.get_post_risk_type, name='get_post_risk_type'),
+]

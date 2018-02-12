f4535d4ae790fb4acbdd5501258eb470991a0184
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-12 15:23:25 -0200

    Risk router added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,12 +3,14 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from risk_types import views as risk_types_views
+from risks import views as risk_views
 from field_types import views as field_types_views
 from fields import views as fields_views
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register('risk-types', risk_types_views.RiskTypeViewSet)
+router.register('risk', risk_views.RiskViewSet)
 router.register('field-types', field_types_views.FieldTypesViewSet)
 router.register('field', fields_views.FieldViewSet)
 

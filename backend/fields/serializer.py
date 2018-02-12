1bcd0608f62d1a1d572ba6094568b934e06d2b24
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-12 13:20:30 -0200

    Some future information about relationshipt added to field serializer

--- backend/fields/serializer.py
+++ backend/fields/serializer.py
@@ -1,7 +1,11 @@
 from .models import Field
 from rest_framework import serializers
-
+from field_types.serializer import FieldTypeSerializer
 class FieldSerializer(serializers.ModelSerializer):
+    # field_type = serializers.PrimaryKeyRelatedField(read_only=True)
+    # field_type = FieldTypeSerializer(read_only=True)
+
     class Meta:
         model = Field
         fields =  '__all__'
+        # fields =  ('field_type', 'name', 'description', 'required')

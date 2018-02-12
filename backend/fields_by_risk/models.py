a17e4b2ab916a7e7916f9fe8587fe13da1598140
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-12 20:59:26 -0200

    Migrations and model for fieldsbyrisk created

--- backend/fields_by_risk/models.py
+++ backend/fields_by_risk/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from fields.models import Field
+from risks.models import Risk
 
-# Create your models here.
+class FieldByRisk(models.Model):
+    """ Defines all fields by risk """
+    field = models.ForeignKey(Field, null=False, on_delete=models.CASCADE)
+    risk = models.ForeignKey(Risk, null=False, on_delete=models.CASCADE)

48da3ccf17084bbf4940ba647f31cbcdf8c383d7
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-12 15:14:05 -0200

    Model for risks created

--- backend/risks/models.py
+++ backend/risks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from risks.models import RiskType
 
-# Create your models here.
+class Risk(models.Model):
+    """ Defines all risks """
+    risk_type = models.ForeignKey(RiskType, null=False, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=255, null=True)
+
+    def __repr__(self):
+        return self.name + ' is added.'

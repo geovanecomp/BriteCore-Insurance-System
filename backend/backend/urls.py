eec7e6ebc1243923870cc8b5e9654973b6e221c4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-12 14:42:35 -0200

    Routers determined automatically by the router

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from risk_types import views as risk_types_views
+from field_types import views as field_types_views
+from fields import views as fields_views
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register('risk-types', risk_types_views.RiskTypeViewSet)
+router.register('field-types', field_types_views.FieldTypesViewSet)
+router.register('field', fields_views.FieldViewSet)
+
+# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('risk-types/', include('risk_types.urls')),
-    path('field-types/', include('field_types.urls')),
-    path('field/', include('fields.urls'))
+    path('', include(router.urls)),
 ]

d72649e0f6edfa475cb4c3c8d5a23e9f241bc33e
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-17 16:47:13 -0200

    Setting a route alias to list risks by type

--- backend/risks/views.py
+++ backend/risks/views.py
@@ -12,7 +12,7 @@ class RiskViewSet(viewsets.ModelViewSet):
     queryset = Risk.objects.all()
     serializer_class = RiskSerializer
 
-    @list_route()
+    @list_route(url_path='list-by-risk-type')
     def list_by_risk_type(self, request):
         print ('ROUTE HIT')
         risk_type_id = self.request.query_params.get('risk_type_id', None)
@@ -20,7 +20,7 @@ class RiskViewSet(viewsets.ModelViewSet):
 
         if risk_type_id is not None:
             risks = Risk.objects.filter(risk_type=risk_type_id)
-            serializer = RiskSerializer(risks, many=True)            
+            serializer = RiskSerializer(risks, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_204_NO_CONTENT)

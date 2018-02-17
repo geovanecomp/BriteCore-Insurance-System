952223733e1596abad4989d2154b711f82f944ee
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-17 16:33:55 -0200

    Service to list all risks by type created

--- backend/risks/views.py
+++ backend/risks/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route, list_route
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Risk
 from .serializer import RiskSerializer
@@ -8,3 +11,33 @@ class RiskViewSet(viewsets.ModelViewSet):
 
     queryset = Risk.objects.all()
     serializer_class = RiskSerializer
+
+    @list_route()
+    def list_by_risk_type(self, request):
+        print ('ROUTE HIT')
+        risk_type_id = self.request.query_params.get('risk_type_id', None)
+        print (' PARAM risk_type_id: ', risk_type_id)
+
+        if risk_type_id is not None:
+            risks = Risk.objects.filter(risk_type=risk_type_id)
+            serializer = RiskSerializer(risks, many=True)            
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+        # return super(RiskViewSet, self).list(request)
+
+    # def get_queryset(self):
+    #     """ Get a risk by the parameter """
+    #     risk_type_id = self.kwargs['risk_type_id']
+    #     return Risk.objects.filter(risk_type=risk_type_id)
+
+    # def get_queryset(self):
+    #     """ Get a risk by the parameter """
+    #     queryset = Risk.objects.all()
+    #     risk_type_id = self.request.query_params.get('risk_type_id', None)
+    #     if risk_type_id is not None:
+    #         queryset = queryset.filter(risk_type=risk_type_id)
+    #     return queryset

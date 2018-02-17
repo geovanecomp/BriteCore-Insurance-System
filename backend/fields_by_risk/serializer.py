69a3238ba57e9e25c0f5762570ba911f3a69d42d
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-17 18:55:32 -0200

    Field by risk erializer

--- backend/fields_by_risk/serializer.py
+++ backend/fields_by_risk/serializer.py
@@ -1,7 +1,12 @@
 from .models import FieldByRisk
 from rest_framework import serializers
+from risks.serializer import RiskSerializer
+from fields.serializer import FieldSerializer
 
 class FieldByRiskSerializer(serializers.ModelSerializer):
+    field = FieldSerializer(read_only=True)
+    risk = RiskSerializer(read_only=True)
+
     class Meta:
         model = FieldByRisk
         fields =  '__all__'
